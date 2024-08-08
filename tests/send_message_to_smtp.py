f28b68fa0e7025a4962490cb3e270a26e423793c
Matthias Wiesner <matthias.wiesner@hpi.de>
2024-08-08 17:02:43 +0200

    Add test for smtp

---
+++ tests/send_message_to_smtp.py
@@ -0,0 +1,31 @@
+import click
+import smtplib
+from systemd import journal
+from datetime import datetime, timedelta
+
+
+@click.command()
+@click.option('--filepath', default='./testmail', help='Filepath to bounced email')
+def test(filepath):
+    with open(filepath) as f:
+        message = f.read()
+
+    try:
+        with smtplib.SMTP('127.0.0.1', 2525) as server:
+            server.sendmail(
+                'bouncedemails@bouncedemails.aux.xopic.de',
+                'bouncedemails@bouncedemails.aux.xopic.de',
+                message)
+        print('Testmail sent')
+        print('---')
+        print('Journallog entries')
+        j = journal.Reader()
+        j.seek_realtime(datetime.now() - timedelta(minutes=1))
+        for entry in j:
+            print(entry["MESSAGE"])
+
+    except smtplib.SMTPException as e:
+        print('SMTP error occurred: ' + str(e))
+
+if __name__ == 'main':
+    test()
